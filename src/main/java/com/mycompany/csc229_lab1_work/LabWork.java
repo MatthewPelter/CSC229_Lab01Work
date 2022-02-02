d2ba2c9f76524309b0a849ea763d027ae66bd545
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2022-02-02 13:14:56 -0500

    Task 1

--- src/main/java/com/mycompany/csc229_lab1_work/LabWork.java
+++ src/main/java/com/mycompany/csc229_lab1_work/LabWork.java
@@ -1,7 +1,29 @@
 package com.mycompany.csc229_lab1_work;
 
 public class LabWork {
+
+    static int sumOfAllPrime(int n) {
+        int sum = 0;
+        boolean isPrime = true;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+
+    static boolean isPrime(int x) {
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("The prime sum to 10 is " + sumOfAllPrime(10));
     }
 }
